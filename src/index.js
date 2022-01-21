4231a388ac3415620670a9827daaa71ebdb7abf0
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-21 15:56:02 -0600

    feat: JSX classNames & css implementation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 //JSX element
   const fName =  "Anthony";
-    const lName = "Villegas";
+  const lName = "Villegas";
+  const year = new Date().getFullYear();
+
+//Advanced JSX
+function images(e) {
+    e.target.setAttribute('src', "https://images.idgesg.net/images/article/2019/05/java_binary_code_gears_programming_coding_development_by_bluebay2014_gettyimages-1040871468_2400x1600-100795798-large.jpg?auto=webp&quality=85,70");
+    e.target.setAttribute('alt', 'java');
+
+}
+
 
 
 //JSX EXPRESSION
@@ -17,13 +26,18 @@ const myDiv = (
               Your To-Do List {`${fName} ${lName}`} !
         </h1>
         <ul>
+
             <li>Drink Coffee</li>
             <li>Learn React</li>
+            <img onClick={images} src="https://cdn.evilmartians.com/front/posts/optimizing-react-virtual-dom-explained/cover-a1d5b40.png" alt="react"/>
             <li>Learn Java</li>
             <li>Eat food</li>
             <li>Land Dream Job</li>
         </ul>
         <p>Your Lucky number for today is {Math.floor(Math.random() * 10)}</p>
+        <p>Created by {fName} {lName} <span>&copy; {year}</span></p>
+
+
     </div>
 );
 
